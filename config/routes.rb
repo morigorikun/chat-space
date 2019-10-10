8cb05c59f647a9b3969d4bd25611f2b1ffc365f8
morigorikun <morigorikun@iCloud.com>
2019-10-10 21:40:11 +0900

    ユーザー情報編集機能の追加
    ・users.controller.rbを作成。rails g controller users
    editとupdateを追記する
      private
    
      def user_params
        params.require(:user).permit(:name, :email)
      end
    
      privateを追記してuser_paramsを追記する。
    ・shared/_side_bar.html.haml部分テンプレを作成。
    ・routes.rbにusersを作成しedit.updateを追加する
    ・users/edit.html.hamlを編集。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'messages#index'
+  resources :users, only: [:edit, :update]
   # resources :messages, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
