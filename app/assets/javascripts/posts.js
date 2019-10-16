682c94b02a8d6e909910a413c6b70375afdb9e0f
morigorikun <morigorikun@iCloud.com>
2019-10-17 01:40:49 +0900

    posts.js
    posts.jsの編集

---
+++ app/assets/javascripts/posts.js
@@ -0,0 +1,16 @@
+$(function(){
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    console.log(this)
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      datatype: 'json',
+      processData: false,
+      contentType:  false 
+    })
+  })
+});
