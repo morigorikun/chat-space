593f2460349f546e7c45c885260bc551d2b01c64
morigorikun <morigorikun@iCloud.com>
2019-10-22 00:40:43 +0900

    テキストフィールドに入力されるたびにイベントが発火するようにする
    message.jsの編集

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){
-  $(".chat__group_name").on("keyup", function(){
-    var input = $(".chat__group_name").val();    //フォームの値を取得して変数に代入する
+  $("#user-search-field").on("keyup", function(){
+    var input = $("#user-search-field").val();    //フォームの値を取得して変数に代入する
     console.log(input);
   });
 });
