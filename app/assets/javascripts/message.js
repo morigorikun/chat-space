90b4c7c3f4b5af6ee89c8de92732610ad034efb2
morigorikun <morigorikun@iCloud.com>
2019-10-23 16:16:06 +0900

    非同期通信
    htmlの作成
    forEachの記述

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,12 +1,46 @@
 $(function(){
-  $("#user-search-field").on("keyup", function(){
-    var input = $("#user-search-field").val();    //フォームの値を取得して変数に代入する
+
+  var search_list = $("#user-search-result");
+  
+function appendUser(user)  {
+  var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${ user.name }</p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
+              </div>`;
+  search_list.append(html);
+}
+
+function appendNoUser(user){
+  var html = `<div class='chat-group-user clearfix'>
+                <p class="chat-group-user__name">${user}が見つかりません</p>
+              </div>`
+  search_list.append(html);
+}
+
+  $(".chat-group-form__input").on("keyup", function(){
+    var input = $(this).val();    //フォームの値を取得して変数に代入する
+
   
     $.ajax({
       type: 'GET',
-      url: '/users/index',
+      url: '/users',
       dataType: 'json',
       data: { keyword: input },
     })
+    .done(function(users) {
+      console.log(users)
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else {
+        appendNoUser("該当するユーザーがいません");
+      }
+    })
+    .fail(function() {
+      alert('エラー');
+    });
   });
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   # resources :messages, only: :index
